7cf5678ed18ebb38a1e5070a18b86f83b58da30a
Nick Østergaard <nickoe@es.aau.dk>
2012-09-10 15:05:38 +0200

    Blinky test with the Arduino Mega 2560 that works

--- sw/lli/lli.c
+++ sw/lli/lli.c
@@ -6,11 +6,13 @@ int main (void)
 {
   /* set PORTB.5 for output*/
   DDRB = (1<<PB5);
+  DDRB = (1<<PB7);
  
   while (1)
     {
       /* flip PORTB.2 */
       PORTB ^= (1<<PB5);
+      PORTB ^= (1<<PB7);
 	  	_delay_ms(100);
     }
  
