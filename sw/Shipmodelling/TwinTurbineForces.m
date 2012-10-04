16e32fd8e9dd0edadca74d39d179e0d6724004b3
Frederik Juul <Frederikj@gmail.com>
2012-10-04 15:34:31 +0200

    Added file which calculates resulting forces from motor forces

---
+++ sw/Shipmodelling/TwinTurbineForces.m
@@ -0,0 +1,22 @@
+clc
+safety=inline('abs(n)<=pi/2');
+theta=pi/2
+Fl = -4;
+Fr = 4;
+Ftl = cos(theta)*Fl;
+Ftr = cos(-theta)*Fr;
+Fxl = cos(theta)*Ftl;
+Fxr = cos(-theta)*Ftr;
+Fx = safety(theta)*(Fxr+Fxl)
+Fyl = Ftl-Fxl;
+Fyr = Ftr-Fxr;
+Fy = safety(theta)*(Fyl-Fyr)
+
+
+FRl=Fl-Ftl;
+FRr=Fr-Ftr;
+FR=safety(theta)*(FRr-FRl)
+
+%% Twin Turbine
+
+ftl-fl
