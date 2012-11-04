b74837eda8a0fec2bf87308c3003c17994db8293
Attila Fodor <attila.fodor.89@gmail.com>
2012-11-04 20:05:39 +0100

    An update on the Map reading.

--- sw/HLI/Python/Course_keeping_control/src/OSMTest.py
+++ sw/HLI/Python/Course_keeping_control/src/OSMTest.py
@@ -15,37 +15,47 @@ MapObj = Osm.OsmApi();
 '''Laeso'''
 #Terkep = MapObj.Map(57.187, 10.826, 57.335, 11.214);
 Terkep = MapObj.Map(11.1668, 57.2774, 11.1824, 57.2842);
+Terkep = MapObj.Map(-36.432, 65.675, -35.99, 66.029);
 
-longitudes = numpy.zeros(len(Terkep)-3);
-latitudes = numpy.zeros(len(Terkep)-3);
-ids = numpy.zeros(len(Terkep)-3);
 i = 0;
+n = 0; ''' Number of Ways'''
+ids = numpy.zeros(1000); ''' Way IDs'''
+m = 0;
 
-while i < len(Terkep)-3:
+while i < len(Terkep):
+    if 'nd' in Terkep[i]['data'].keys():
+        ids[n] = Terkep[i]['data']['id'];
+        n = n+1;
+    i = i+1;
+
+#print(ids)
     
-    longitudes[i] = Terkep[i]['data']['lon'];
-    latitudes[i] = Terkep[i]['data']['lat'];
-    ids[i] = Terkep[i]['data']['id'];
-    i = i + 1;
-   
+while m < n:
+    if ids[m] == 0:
+        break;
     
-id_start = min(ids);
+    wayObj = MapObj.WayGet(ids[m]);
+    
+    way = wayObj['nd'];
+    
+    longitudes = numpy.zeros(len(way));
+    latitudes = numpy.zeros(len(way));
+    
+    i = 0;
+    
+    while i < len(way):
+        node = MapObj.NodeGet(way[i]);
+        #print(node);
+        longitudes[i] = node['lon'];
+        latitudes[i] = node['lat'];
+        i = i + 1;        
+        #print(i);
+    
+    
+    #print(ids);
+    plt.plot(longitudes,latitudes, 'k');
+    
+    m = m + 1;
 
-print(Terkep[i]['data']['nd']);
-
-way = Terkep[len(Terkep)-3]['data']['nd'];
-longitudes_2 = numpy.zeros(len(Terkep)-3);
-latitudes_2 = numpy.zeros(len(Terkep)-3);
-i = 0;
-
-while i < len(Terkep)-3:
-    longitudes_2[i] = 0.5 * longitudes[FL.find_id_index(ids, way[i])];
-    latitudes_2[i] = latitudes[FL.find_id_index(ids, way[i])];
-    i = i + 1;        
-    print(i);
-
-
-print(ids);
-plt.plot(longitudes_2,latitudes_2);
-plt.axes().set_aspect('equal');
+#plt.axes().set_aspect('equal'); 
 plt.show();
