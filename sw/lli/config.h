98bf3e1a041b2ed8da72145bb4174e7603478cbc
Nick Østergaard <nickoe@es.aau.dk>
2013-07-11 10:15:23 +0200

    Comments in config.h

--- sw/lli/config.h
+++ sw/lli/config.h
@@ -21,7 +21,7 @@ extern uint8_t rmc_idx;
  */
 // USB connection
 #define UART_BAUD_RATE   115200
-// APC220 radio
+// APC220/RF7020 radio
 #define UART2_BAUD_RATE   38400
 // UP-501 GPS
 #define UART3_BAUD_RATE    9600
@@ -56,10 +56,20 @@ extern uint8_t rmc_idx;
  */
 #define MAX_MSG_SIZE 1024  // This should probably be 255 + len + devid + msgid + checksum
 #define TX_BUFF_SIZE 350
+
+/* Defines sample rate from the ADIS16405 bt counting interrupt ticks
+ * from sample ready pin on device */
 //#define ADIS_READY 164 // 10 Hz
 #define ADIS_READY 82 // 20Hz
 //#define ADIS_READY 42 // 40Hz
+
+/* A hacky timer used to be able to send data in bulk, such the radio
+ * can have time to switch from RX to TX and vice versa, to gain
+ * better throughput */
 #define TX_READY 546 // 3Hz
+
+/* Defines for how many control input packets we can loose without it
+ * shutting down */
 #define AWAKE_THRESHOLD 3
 
 #endif // CONFIG_H 
